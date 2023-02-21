0c44710a01339db78d62243d6c79c6c0d9a3a3b0
Andrew Ernest <anernest@ratesresearch.org>
2023-02-21 14:28:17 +0000

    change to dj_drf_jwt

--- dj_drf_jwt/apps.py
+++ dj_drf_jwt/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
-from django_drf_jwt.settings import *
+from dj_drf_jwt.settings import *
 
 class DjangoDrfJwtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_drf_jwt'
+    name = 'dj_drf_jwt'
     
